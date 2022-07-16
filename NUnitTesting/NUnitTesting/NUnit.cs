c65cda147db99f39614da1ba93071241349f4ebb
Sourav2707 <smartsourav33@gmail.com>
2022-07-16 13:38:34 +0530

    [Sourav] - Binary using Nibbles

--- NUnitTesting/NUnitTesting/NUnit.cs
+++ NUnitTesting/NUnitTesting/NUnit.cs
@@ -11,14 +11,39 @@ namespace NUnitTesting
         public static void Test(int n)
         {
             String bin = "";
-            Console.Write($"Binary of {n} is ");
-            while (n > 0)
+            Console.Write("Binary of " + n + " is ");
+            while (n > 0)                         //finding the binary number
             {
                 bin = n % 2 + bin;
                 n = n / 2;
             }
-
-            Console.Write(bin);
+            Console.WriteLine(bin);
+            NUnit.swapnibble(bin);
+        }
+        public static void swapnibble(string binary)
+        {
+            while (binary.Length < 8)                  //So that we can get a 8 digit binary code
+            {
+                binary = "0" + binary;
+            }
+            Console.WriteLine(binary);
+            String swap = binary.Substring(4, 4) + binary.Substring(0, 4);          //substring function to swap the nibbles
+            Console.WriteLine("Swapped binary is " + swap);
+            NUnit.toDecimal(swap);
+        }
+        public static void toDecimal(string swapped)
+        {
+            int binary = Convert.ToInt32(swapped);                      //converting swapped string to integer format
+            int Decimal = 0;
+            int n = 0;                                                      //power of two for coverting to decimal
+            while (binary != 0)                                    //converting to decimal
+            {
+                int deci = (int)Math.Pow(2, n) * (binary % 10);
+                Decimal = Decimal + deci;
+                binary = binary / 10;
+                n++;
+            }
+            Console.WriteLine("Decimal for " + swapped + " is " + Decimal);
         }
     }
 }
--- NUnitTesting/NUnitTesting/Program.cs
+++ NUnitTesting/NUnitTesting/Program.cs
@@ -1,5 +1,5 @@
 ﻿using NUnitTesting;
 Console.WriteLine("Welcome to NUnit Testing peogram");
-Console.WriteLine("Enter a number whose binary you want to find");
+Console.WriteLine("Enter a number whose binary's nibble swapped decimal you want");
 int n = Convert.ToInt32(Console.ReadLine());
 NUnit.Test(n);
